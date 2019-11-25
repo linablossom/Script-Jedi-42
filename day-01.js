f82667e6c3a13c044fb008563ad42bb15560a1ca
Alina34-a <56880310+Alina34-a@users.noreply.github.com>
2019-11-25 10:58:22 +0200

    Create day-01.js

---
+++ day-01.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/training-js-number-1-create-your-first-js-function-and-print-helloworld/train/javascript
+
+function helloWorld() {
+  var str = "Hello world!";
+  console.log(str);
+  };
