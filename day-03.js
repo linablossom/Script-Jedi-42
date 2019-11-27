c99febecf6ab3bd97749e53b85663cb9747f3e2e
Alina34-a <56880310+Alina34-a@users.noreply.github.com>
2019-11-27 02:03:19 +0200

    Update day-03.js

--- day-03.js
+++ day-03.js
@@ -1,2 +1,4 @@
-http://www.codewars.com/kata/571f832f07363d295d001ba8
-const trueOrFalse = (val) => !val ? 'false' : 'true';
+// http://www.codewars.com/kata/571f832f07363d295d001ba8
+const trueOrFalse = val => !val ? 'false' : 'true';
+// http://www.codewars.com/kata/57202aefe8d6c514300001fd
+const saleHotdogs = n => n * (n < 5 ? 100 : n < 10 ? 95 : 90);
