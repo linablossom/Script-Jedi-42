eac8673654f7ea72689cc09fedf572c66f75888d
Alina34-a <56880310+Alina34-a@users.noreply.github.com>
2019-11-27 04:01:10 +0200

    Update day-03.js

--- day-03.js
+++ day-03.js
@@ -2,3 +2,11 @@
 const trueOrFalse = val => !val ? 'false' : 'true';
 // http://www.codewars.com/kata/57202aefe8d6c514300001fd
 const saleHotdogs = n => n * (n < 5 ? 100 : n < 10 ? 95 : 90);
+// http://www.codewars.com/kata/572059afc2f4612825000d8a
+// хотіла коротше записати, але без switch цю задачу не пропускає
+const howManydays = month => {
+  switch (month) {
+    default: 
+    return new Date(new Date().getFullYear(), month, 0).getDate();
+  }
+};
