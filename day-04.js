f17ddf6666b490d5b27538e7a4ed85f6bbab3b71
Alina34-a <56880310+Alina34-a@users.noreply.github.com>
2019-11-28 18:02:16 +0200

    Update day-04.js

--- day-04.js
+++ day-04.js
@@ -1,8 +1,23 @@
 // http://www.codewars.com/kata/57216d4bcdd71175d6000560
 const padIt = (str, n) => {
-  let str1 = str, count = 0;
+  let res = str, count = 0;
   while (count++ < n) {
-    str1 = count % 2 === 0 ? str1 + "*" : "*" + str1;
+    res = count % 2 === 0 ? res + "*" : "*" + res;
   } 
-  return str1;
+  return res;
+};
+// http://www.codewars.com/kata/5721a78c283129e416000999
+const pickIt = arr => {
+  cnst odd = [], even = [];
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] % 2 === 0 ? even.push(arr[i]) : odd.push(arr[i]);
+  }
+  return [odd, even];
+};
+const pickIt = arr => {
+  const odd = [], even = [];
+  for (let res of arr) {
+    ((res % 2) ? odd : even).push(res);
+  }
+  return [odd, even];
 };
