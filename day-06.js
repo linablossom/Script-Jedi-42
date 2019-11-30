9bf36925c164408b278e19f398da5edbd9289530
Alina34-a <56880310+Alina34-a@users.noreply.github.com>
2019-11-30 18:49:10 +0200

    Update day-06.js

--- day-06.js
+++ day-06.js
@@ -1,2 +1,6 @@
 // https://www.codewars.com/kata/maximum-multiple/train/javascript
 const maxMultiple = (divisor, bound) => divisor * Math.trunc(bound / divisor);
+// https://www.codewars.com/kata/is-he-gonna-survive/train/javascript
+const hero = (bullets, dragons) => bullets / dragons >= 2;
+// https://www.codewars.com/kata/59441520102eaa25260000bf
+const unusualFive = () => "Alina".length;
