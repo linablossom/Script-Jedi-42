638668391ee91b6521731b99ace017716e0f245d
Alina34-a <56880310+Alina34-a@users.noreply.github.com>
2019-12-03 20:50:30 +0200

    Update day-09.js

--- day-09.js
+++ day-09.js
@@ -8,3 +8,17 @@ function cutIt(arr) {
   
   return arr.map(item => item.slice(0, minLength));
 }
+
+// http://www.codewars.com/kata/57277a31e5e51450a4000010
+const firstToLast = (str, c) => {
+  const firstIndex = str.indexOf(c);
+  if (firstIndex === -1) return -1;
+  const lastIndex = str.lastIndexOf(c);
+  if (lastIndex === -1) return 0;
+  
+  return lastIndex - firstIndex;
+}
+
+// https://www.codewars.com/kata/57280481e8118511f7000ffa
+const splitAndMerge = (str, sp) => 
+  str.split(' ').map(word => word.split('').join(sp)).join(' ');
