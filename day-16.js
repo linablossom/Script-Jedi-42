4221d3d64fda7e6d9ec31325a85d64fd982e47c7
Alina34-a <56880310+Alina34-a@users.noreply.github.com>
2019-12-15 12:08:45 +0200

    Update day-16.js

--- day-16.js
+++ day-16.js
@@ -16,4 +16,30 @@ const sortIt = arr => {
 const isolateIt = arr => arr.map(x => 
   x.slice(0, Math.floor(x.length / 2)) + "|" + x.slice(-Math.floor( x.length / 2)));
 // http://www.codewars.com/kata/573023c81add650b84000429
+const grades = [
+  ['S', 100, 100],
+  ['A', 90, 99],
+  ['B', 80, 89],
+  ['C', 60, 79],
+  ['D', 0, 59],
+  ['X', -1, -1]
+];
+
+const countGrade = scores =>
+  grades.reduce((res, [letter, minValue, maxValue]) => {
+    res[letter] = scores.filter(score => score >= minValue && score <= maxValue).length;
+    
+    return res;
+  }, {});
 // http://www.codewars.com/kata/57308546bd9f0987c2000d07
+function mirrorImage(arr){
+  let match = [-1, -1];
+  arr.forEach((num, numIndex) => {
+    const oppositeNum = +(num + '').split('').reverse().join('');
+    if (arr.find((item, itemIndex) => itemIndex > numIndex && item === oppositeNum)) {
+      match = [num, oppositeNum];
+    }
+  });
+  
+  return match;
+}
