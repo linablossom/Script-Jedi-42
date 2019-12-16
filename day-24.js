5369c2c7d68d96f08c27c5e7dd227ebdf94e0b46
Alina34-a <56880310+Alina34-a@users.noreply.github.com>
2019-12-16 19:16:57 +0200

    Update day-24.js

--- day-24.js
+++ day-24.js
@@ -1,3 +1,11 @@
 // http://www.codewars.com/kata/duplicate-arguments
-
+function solution (...args) {
+  for (let i = 0; i < args.length - 1; i++) {
+    if (args.findIndex((item, index) => item === args[i] && index > i) !== - 1) {
+      return true;
+    }
+  }
+  
+  return false;
+}
 // http://www.codewars.com/kata/last
