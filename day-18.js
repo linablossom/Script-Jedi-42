973d8e11e76d7f9cd199620e63dadaf4447d34bf
Alina34-a <56880310+Alina34-a@users.noreply.github.com>
2019-12-16 19:42:14 +0200

    Create day-18.js

---
+++ day-18.js
@@ -0,0 +1,15 @@
+// http://tddbin.com/#?kata=es6/language/array-api/from
+
+// http://tddbin.com/#?kata=es6/language/array-api/of
+
+// http://tddbin.com/#?kata=es6/language/array-api/fill
+
+// http://tddbin.com/#?kata=es6/language/array-api/find
+
+// http://tddbin.com/#?kata=es6/language/array-api/findIndex
+
+// http://tddbin.com/#?kata=es6/language/array-api/entries
+
+// http://tddbin.com/#?kata=es6/language/array-api/keys
+
+// http://tddbin.com/#?kata=es6/language/array-api/values
