d2130bc10aea5efdf93066aebc0e1261f9b6f1a8
Alina34-a <56880310+Alina34-a@users.noreply.github.com>
2019-12-16 19:44:04 +0200

    Create day-19.js

---
+++ day-19.js
@@ -0,0 +1,11 @@
+// http://tddbin.com/#?kata=es6/language/template-strings/basics
+
+// http://tddbin.com/#?kata=es6/language/template-strings/multiline
+
+// http://tddbin.com/#?kata=es6/language/template-strings/tagged
+
+// http://tddbin.com/#?kata=es6/language/template-strings/raw
+
+// http://tddbin.com/#?kata=es6/language/spread/with-arrays
+
+// http://tddbin.com/#?kata=es6/language/spread/with-strings
