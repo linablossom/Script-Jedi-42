90e87208644fa30d60b23de255cff2204cd299c3
Alina34-a <56880310+Alina34-a@users.noreply.github.com>
2019-12-16 19:58:21 +0200

    Update day-24.js

--- day-24.js
+++ day-24.js
@@ -9,3 +9,15 @@ function solution (...args) {
   return false;
 }
 // http://www.codewars.com/kata/last
+function last (...list) {
+  if (list.length > 1) {
+    return list[list.length - 1];
+  }
+  if (typeof list[0] === 'string') {
+    const chars = list[0].split('');
+    return chars[chars.length - 1];
+  }
+  if (Array.isArray(list[0])) {
+    return list[0][list[0].length - 1];
+  }
+}
