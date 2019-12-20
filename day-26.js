84654ffc75dc56ede4573e7798be041604fb69f8
Alina34-a <56880310+Alina34-a@users.noreply.github.com>
2019-12-20 23:55:33 +0200

    Create day-26.js

---
+++ day-26.js
@@ -0,0 +1,6 @@
+// http://www.codewars.com/kata/power-bind
+
+// https://www.codewars.com/kata/sort-with-arrow-functions
+var OrderPeople = function(people){
+  return people.sort((a, b) => a.age - b.age);
+}
